64bb5dd493ff22d0fa44d48413b4de60c4dba3b5
Joe DeCapo <joe@polka.cat>
2020-12-06 16:48:57 -0600

    Add subcommand to app

--- pygoodle/app.py
+++ pygoodle/app.py
@@ -7,62 +7,75 @@
 import argparse
 import pkg_resources
 from subprocess import CalledProcessError
-from typing import Callable, List, Optional, Tuple
+from typing import Callable, List, Optional, Tuple, Union
 
 import argcomplete
 import colorama
 from trio import MultiError
 
-from pygoodle.cli import add_parser_arguments
 from pygoodle.console import CONSOLE
 
 
-ParserArguments = List[Tuple[List[str], dict]]
+Parser = Union[argparse.ArgumentParser, argparse._MutuallyExclusiveGroup, argparse._ArgumentGroup]  # noqa
+Arguments = List[Tuple[List[str], dict]]
+
+
+class Subcommand(object):
+
+    def __init__(self, name: str, help_message: str, args: Optional[Arguments] = None,
+                 mutually_exclusive_args: Optional[List[Arguments]] = None,
+                 subcommands: Optional[List['Subcommand']] = None):
+        self.name: str = name
+        self.help_message: str = help_message
+        self.args: Optional[Arguments] = args
+        self.mutually_exclusive_args: Optional[List[Arguments]] = mutually_exclusive_args
+        self.subcommands: Optional[List[Subcommand]] = subcommands
+
+    def run(self, args) -> None:
+        raise NotImplementedError
 
 
 class App(object):
 
     def __init__(self, name: str, entry_point: Optional[str] = None,
-                 arguments: Optional[ParserArguments] = None,
-                 subcommands: Optional[List[Callable]] = None):
+                 arguments: Optional[Arguments] = None,
+                 mutually_exclusive_args: Optional[List[Arguments]] = None,
+                 subcommands: Optional[List[Subcommand]] = None):
         from rich.traceback import install
         install()
         colorama.init()
 
         self.name = name
         self.entry_point = name if entry_point is None else entry_point
-        self.parser: argparse.ArgumentParser = self._create_parser(arguments)
+        self.parser: argparse.ArgumentParser = self._create_parser(arguments, mutually_exclusive_args)
         self.subparsers = self.parser.add_subparsers(dest='subcommand')
-        if subcommands is not None:
-            for subcommand in subcommands:
-                subcommand(self.subparsers)
+        for subcommand in subcommands:
+            self.add_subcommand(subcommand)
 
-    def _create_parser(self, arguments: Optional[ParserArguments] = None) -> argparse.ArgumentParser:
-        """Configure CLI parsers
+    def add_subcommand(self, subcommand: Subcommand, parser: Optional[Parser] = None) -> None:
+        """Add arguments to parser
 
-        :return: Configured argument parser for command
-        :rtype: argparse.ArgumentParser
+        :param Subcommand subcommand: Subcommand object
+        :param Optional[Parser] parser: Parser to add subcommand to
         """
 
-        def command_help(_):
-            """command help handler"""
+        parser = self.subparsers if parser is None else parser
+        sub_parser = parser.add_parser(subcommand.name, help=subcommand.help_message)
+        sub_parser.formatter_class = argparse.RawTextHelpFormatter
 
-            command_parser.print_help()
+        if subcommand.args is not None:
+            self._add_parser_arguments(sub_parser, subcommand.args)
 
-        try:
-            command_parser = argparse.ArgumentParser(prog=self.entry_point)
-            command_parser.set_defaults(func=command_help)
-            version_message = f"{self.entry_point} version {pkg_resources.require(self.name)[0].version}"
-            add_parser_arguments(command_parser, [
-                (['-v', '--version'], dict(action='version', version=version_message))
-            ])
-            if arguments is not None:
-                add_parser_arguments(command_parser, arguments)
+        if subcommand.mutually_exclusive_args is not None:
+            for args in subcommand.mutually_exclusive_args:
+                self._add_parser_arguments(sub_parser.add_mutually_exclusive_group(), args)
 
-            return command_parser
-        except Exception:
-            CONSOLE.stderr('Failed to create parser')
-            raise
+        if subcommand.subcommands is None:
+            sub_parser.set_defaults(func=sub_parser.print_help)
+        else:
+            sub_parser.set_defaults(func=subcommand.run)
+            for command in subcommand.subcommands:
+                self.add_subcommand(command, sub_parser)
 
     def run(self, process_args: Callable = lambda _: None) -> None:
         """command CLI main function"""
@@ -97,3 +110,42 @@ class App(object):
             CONSOLE.stderr('** Unhandled exception **')
             CONSOLE.print_exception()
             exit(1)
+
+    @staticmethod
+    def _add_parser_arguments(parser: Parser, arguments: Arguments) -> None:
+        """Add arguments to parser
+
+        :param Parser parser: Parser to add arguments to
+        :param Arguments arguments: Arguments to add to parser
+        """
+
+        for argument in arguments:
+            parser.add_argument(*argument[0], **argument[1])
+
+    def _create_parser(self, arguments: Optional[Arguments] = None,
+                       mutually_exclusive_args: Optional[List[Arguments]] = None) -> argparse.ArgumentParser:
+        """Configure CLI parsers
+
+        :return: Configured argument parser for command
+        :rtype: argparse.ArgumentParser
+        """
+
+        try:
+            command_parser = argparse.ArgumentParser(prog=self.entry_point)
+            command_parser.set_defaults(func=command_parser.print_help)
+            version_message = f"{self.entry_point} version {pkg_resources.require(self.name)[0].version}"
+            self._add_parser_arguments(command_parser, [
+                (['-v', '--version'], dict(action='version', version=version_message))
+            ])
+
+            if arguments is not None:
+                self._add_parser_arguments(command_parser, arguments)
+
+            if mutually_exclusive_args is not None:
+                for args in mutually_exclusive_args:
+                    self._add_parser_arguments(command_parser.add_mutually_exclusive_group(), args)
+
+            return command_parser
+        except Exception:
+            CONSOLE.stderr('Failed to create parser')
+            raise
