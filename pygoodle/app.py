a2f2b61d29fc450ea9c62f4d11ee2b5ea15b3bf0
Joe DeCapo <joe@polka.cat>
2020-12-07 02:13:18 -0600

    Add reflection

--- pygoodle/app.py
+++ pygoodle/app.py
@@ -13,6 +13,7 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 import argcomplete
 from trio import MultiError
 
+import pygoodle.reflection as reflect
 from .console import CONSOLE
 from .format import Format
 
@@ -56,12 +57,12 @@ Parser = Union[argparse.ArgumentParser, argparse._MutuallyExclusiveGroup, argpar
 class Subcommand(object):
 
     class Meta:
-    name: str = 'subcommand'
-    help: str = f'{name} help'
-    args: List[Argument] = []
-    mutually_exclusive_args: List[List[Argument]] = []
-    argument_groups: Dict[str, List[Argument]] = {}
-    subcommands: List['Subcommand'] = []
+        name: str = 'subcommand'
+        help: str = f'{name} help'
+        args: List[Argument] = []
+        mutually_exclusive_args: List[List[Argument]] = []
+        argument_groups: Dict[str, List[Argument]] = {}
+        subcommands: List['Subcommand'] = []
 
     def __init__(self):
         self._parser: Optional[argparse.ArgumentParser] = None
@@ -71,15 +72,6 @@ class Subcommand(object):
         self.mutually_exclusive_args: List[List[Argument]] = []
         self.argument_groups: Dict[str, List[Argument]] = {}
         self.subcommands: List['Subcommand'] = []
-
-        meta = self._get_meta()
-        if meta is not None:
-            self._update_property('name', meta)
-            self._update_property('help', meta)
-            self._update_property('args', meta)
-            self._update_property('mutually_exclusive_args', meta)
-            self._update_property('argument_groups', meta)
-            self._update_property('subcommands', meta)
 
     @staticmethod
     def run(args) -> None:
@@ -91,17 +83,20 @@ class Subcommand(object):
     def add_parser(self, parser) -> None:
         self._parser = parser
 
-    def _get_meta(self) -> Optional[Meta]:
-        class_dict = self.__class__.__dict__
-        meta = 'Meta'
-        if meta in class_dict:
-            return class_dict[meta]
-        return None
+    def _update_attr(self, name: str, meta: Meta) -> None:
+        reflect.update_attr(self, name, meta)
 
-    def _update_property(self, name: str, meta: Meta):
-        if hasattr(meta, name):
-            meta_value = getattr(meta, name)
-            setattr(self, name, meta_value)
+    def _update_meta(self) -> None:
+        classes = reflect.method_resolution_order(self, reverse=True)
+        for cls in classes:
+            meta = reflect.class_member(cls, 'Meta')
+            if meta is not None:
+                self._update_attr('name', meta)
+                self._update_attr('help', meta)
+                self._update_attr('args', meta)
+                self._update_attr('mutually_exclusive_args', meta)
+                self._update_attr('argument_groups', meta)
+                self._update_attr('subcommands', meta)
 
 
 class App(object):
