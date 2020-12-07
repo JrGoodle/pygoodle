20b0e5fa4ca10eaef8d65981c30b4c56f3f1e6b4
Joe DeCapo <joe@polka.cat>
2020-12-06 19:24:45 -0600

    Refactor app command utils

--- pygoodle/app.py
+++ pygoodle/app.py
@@ -24,42 +24,36 @@ class Argument(object):
 
 
 Parser = Union[argparse.ArgumentParser, argparse._MutuallyExclusiveGroup, argparse._ArgumentGroup]  # noqa
-Arguments = List[Argument]
 
 
 class Subcommand(object):
 
-    def __init__(self, name: str, help_message: str, args: Optional[Arguments] = None,
-                 mutually_exclusive_args: Optional[List[Arguments]] = None,
-                 subcommands: Optional[List['Subcommand']] = None):
-        self.name: str = name
-        self.help_message: str = help_message
-        self.args: Arguments = [] if args is None else args
-        mut_ex_args = [] if mutually_exclusive_args is None else mutually_exclusive_args
-        self.mutually_exclusive_args: List[Arguments] = mut_ex_args
-        self.subcommands: Optional[List[Subcommand]] = subcommands
-        self.add_args()
+    name: str = 'subcommand'
+    help: str = f'{name} help'
+    args: List[Argument] = []
+    mutually_exclusive_args: List[List[Argument]] = []
+    argument_groups: Dict[str, List[Argument]] = []
+    subcommands: List['Subcommand'] = []
 
-    def add_args(self) -> None:
-        pass
-
-    def run(self, args) -> None:
+    @staticmethod
+    def run(args) -> None:
         raise NotImplementedError
 
 
 class App(object):
 
     def __init__(self, name: str, entry_point: Optional[str] = None,
-                 arguments: Optional[Arguments] = None,
-                 mutually_exclusive_args: Optional[List[Arguments]] = None,
-                 subcommands: Optional[List[Subcommand]] = None):
+                 arguments: Optional[List[Argument]] = None,
+                 mutually_exclusive_args: Optional[List[List[Argument]]] = None,
+                 subcommands: Optional[List[Subcommand]] = None,
+                 argument_groups: Optional[Dict[str, List[Argument]]] = None):
         from rich.traceback import install
         install()
         colorama.init()
 
         self.name = name
         self.entry_point = name if entry_point is None else entry_point
-        self.parser: argparse.ArgumentParser = self._create_parser(arguments, mutually_exclusive_args)
+        self.parser: argparse.ArgumentParser = self._create_parser(arguments, mutually_exclusive_args, argument_groups)
         self.subparsers = self.parser.add_subparsers(dest='subcommand')
         for subcommand in subcommands:
             self.add_subcommand(subcommand)
@@ -72,13 +66,16 @@ class App(object):
         """
 
         parser = self.subparsers if parser is None else parser
-        sub_parser = parser.add_parser(subcommand.name, help=subcommand.help_message)
+        sub_parser = parser.add_parser(subcommand.name, help=subcommand.help)
         sub_parser.formatter_class = argparse.RawTextHelpFormatter
 
         self._add_parser_arguments(sub_parser, subcommand.args)
 
         for args in subcommand.mutually_exclusive_args:
             self._add_parser_arguments(sub_parser.add_mutually_exclusive_group(), args)
+
+        for title, args in subcommand.argument_groups.items():
+            self._add_parser_arguments(sub_parser.add_argument_group(title=title), args)
 
         if subcommand.subcommands is None:
             sub_parser.set_defaults(func=sub_parser.print_help)
@@ -122,7 +119,7 @@ class App(object):
             exit(1)
 
     @staticmethod
-    def _add_parser_arguments(parser: Parser, arguments: Arguments) -> None:
+    def _add_parser_arguments(parser: Parser, arguments: List[Argument]) -> None:
         """Add arguments to parser
 
         :param Parser parser: Parser to add arguments to
@@ -132,8 +129,9 @@ class App(object):
         for argument in arguments:
             parser.add_argument(*argument.args, **argument.options)
 
-    def _create_parser(self, args: Optional[Arguments] = None,
-                       mutually_exclusive_args: Optional[List[Arguments]] = None) -> argparse.ArgumentParser:
+    def _create_parser(self, args: Optional[List[Argument]] = None,
+                       mutually_exclusive_args: Optional[List[List[Argument]]] = None,
+                       argument_groups: Optional[Dict[str, List[Argument]]] = None) -> argparse.ArgumentParser:
         """Configure CLI parsers
 
         :return: Configured argument parser for command
@@ -154,6 +152,10 @@ class App(object):
             if mutually_exclusive_args is not None:
                 for args in mutually_exclusive_args:
                     self._add_parser_arguments(command_parser.add_mutually_exclusive_group(), args)
+
+            if argument_groups is not None:
+                for title, args in argument_groups.items():
+                    self._add_parser_arguments(command_parser.add_argument_group(title=title), args)
 
             return command_parser
         except Exception:
