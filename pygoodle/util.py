ff17fe8c8c973104033607d9f34ede23e5f71b74
Joe DeCapo <joe@polka.cat>
2020-12-21 18:53:13 -0600

    Add tuple sort util

--- pygoodle/util.py
+++ pygoodle/util.py
@@ -4,6 +4,14 @@
 
 """
 
+from typing import List, Tuple, TypeVar
+
+T = TypeVar('T')
+
 
 def is_even(number: int) -> bool:
     return (number % 2) == 0
+
+
+def sorted_tuple(files: List[T]) -> Tuple[T, ...]:
+    return tuple(sorted(set(files)))
